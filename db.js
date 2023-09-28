265ff0d4047ee39abd536f8f81f70a9b902569ef
deepak104080 <deepak104080@gmail.com>
2023-09-28 19:03:24 +0530

    DB Details updated

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 module.exports = mysql.createConnection({
     host: '162.241.85.140',
-    user: 'codeapzu_jscmsadmin',
-    password: 'jscmsadmin2023',
-    database: 'codeapzu_jscms'
+    user: 'codeapzu_lmsadmin',
+    password: 'lmsadmin2023',
+    database: 'codeapzu_lms'
 });
