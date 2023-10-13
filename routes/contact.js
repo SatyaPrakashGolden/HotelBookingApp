f639eb164aeb3b896051e9b2241fa964ebcb525a
Ashutosh2604 <ashu.raj167@gmail.com>
2023-10-14 00:35:32 +0530

    JSCMS-30-API--Contact-Update updatecontact

--- routes/contact.js
+++ routes/contact.js
@@ -19,5 +19,24 @@ router.post("/register", async(req,res) => {
         res.status(400).json({message: err});
     }
 });
+// http://localhost:4000/contact/updatecontact
+router.put('/updatecontact', async(req,res) => {
+    try{
+        //console.log(req.body.srno);
+        const {name,email,subject,message,responseStatus,responseMessage,responseNextStep,
+        responseNextPerson,}=req.body;
+        const response = await db.promise()
+        .query(`update contact set name='${name}',email='${email}',subject='${subject}',message='${message}',
+        responseStatus='${responseStatus}',responseMessage='${responseMessage}',responseNextStep='${responseNextStep}',
+        responseNextPerson='${responseNextPerson}'
+        where  srno = ${req.body.srno}`);
+        res.status(400).json(response[0]);
+    }
+    catch(err){
+        res.status(400).json({message: err});
+    }
+});
+
+
 
 module.exports = router;
