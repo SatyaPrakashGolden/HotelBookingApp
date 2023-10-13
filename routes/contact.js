4ea24fcdadb31feed43b53e6f7ed9073b4951ddd
Ashutosh2604 <ashu.raj167@gmail.com>
2023-10-13 23:49:12 +0530

    JSCMS-28-API--Contact-Show-all display alluser

--- routes/contact.js
+++ routes/contact.js
@@ -4,6 +4,18 @@ const router = express.Router();
 
 const db = require('../db');
 
+// http://localhost:4000/contact/alluser
+router.get('/alluser', async(req,res) => {
+    try{
+        const response = await db.promise()
+        .query(`select name,email,subject,message from contact where isactive = 1`);
+        res.status(200).json(response[0]);
+    }
+    catch(err){
+        res.status(400).json({message: err});
+    }
+});
+
 // http://localhost:4000/contact/register
 
 router.post("/register", async(req,res) => {
